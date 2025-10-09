5c5a8abffb1259f01e6d5d8d8c5760ddf45a2d94
chensonbh763 <41866678+chensonbh763@users.noreply.github.com>
2025-10-08 23:13:10 -0300

    Update config.js
    https://script.google.com/macros/s/AKfycbwkP0X6LZTlu_CS459Z3m9SU8SO239kG9kZnUAmbc9uz9Udbn7OV8jeD40WH6XUo-ux/exec

--- aiuber/config.js
+++ aiuber/config.js
@@ -1,1 +1,1 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycby_l2bIDejNMryC6_YJosCVMWXoWqJ1rVMULZTLWiyhyLeBwx2pWcbfCLmMDn6MSFr7bA/exec";
+const scriptURL = "https://script.google.com/macros/s/AKfycbwkP0X6LZTlu_CS459Z3m9SU8SO239kG9kZnUAmbc9uz9Udbn7OV8jeD40WH6XUo-ux/exec";
